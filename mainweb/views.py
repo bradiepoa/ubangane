88ebc96cbb14c2fbbd13c608be7edfc69c84d727
bradiepoa7@gmail.com <bradiepoa7@gmail.com>
2023-12-14 21:30:47 +0300

    modified:   mainweb/views.py

--- mainweb/views.py
+++ mainweb/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime, timedelta
 
 
 def home(request):
-    
-    return render(request,"mainweb/index.html")
+    current_time = datetime.now()
+    expiration_time = current_time + timedelta(days=30)
+
+    if current_time < expiration_time:
+        # Display the normal content
+        return render(request, "mainweb/index.html")
+    else:
+        return HttpResponse("Please contact the developer @Bradie poa")
+
 
 def About(request):
     
